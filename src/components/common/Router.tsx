369413f3bc5dea7387ef162cc6e93ea06e104f56
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-06 19:36:39 +0900

    feat: take my svg

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Error404 from "pages/Error404";
 import Animation from "pages/Animation";
 import Variants from "pages/Variants";
@@ -6,17 +6,20 @@ import Gestures from "pages/Gestures";
 import Drag from "pages/Drag";
 import Motion from "pages/Motion";
 import Scroll from "pages/Scroll";
+import Path from "pages/Path";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="" element={<Navigate to="/1" />} />
         <Route path="/1" element={<Animation />} />
         <Route path="/2" element={<Variants />} />
         <Route path="/3" element={<Gestures />} />
         <Route path="/4" element={<Drag />} />
         <Route path="/5-0" element={<Motion />} />
         <Route path="/5" element={<Scroll />} />
+        <Route path="/6" element={<Path />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
