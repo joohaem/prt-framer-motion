c4424d354a629a82254ec82e6e88a8eecb32da09
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-06 18:38:56 +0900

    feat: 5-0. Motion Value and Transform

--- src/pages/Motion.tsx
+++ src/pages/Motion.tsx
@@ -2,24 +2,33 @@ import { motion, useMotionValue, useTransform } from "framer-motion";
 // import { useEffect } from "react";
 import styled from "styled-components";
 
-export default function Scroll() {
+export default function Motion() {
   // naming for style properties' shortcut
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(135, 226, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg,#e09,#d0e)",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
 
   // useEffect(() => {
   //   x.onChange(() => console.log(x.get()));
   // }, [x]);
 
   return (
-    <StWrapper>
+    <StWrapper style={{ background: gradient }}>
       {/* <button onClick={() => x.set(200)}>click me</button> */}
-      <StBox style={{ x, scale }} drag="x" dragSnapToOrigin />
+      <StBox style={{ x, rotateZ }} drag="x" dragSnapToOrigin />
     </StWrapper>
   );
 }
 
-const StWrapper = styled.div`
+const StWrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
