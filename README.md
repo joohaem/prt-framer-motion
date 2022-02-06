60b9dd1c21b4aa50ced561b359a22aabb3eac3a2
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-06 22:18:25 +0900

    bug: cannot slide to right on clicking prev button (featuring slider)

--- README.md
+++ README.md
@@ -130,6 +130,7 @@
 
 # AnimatePresence
 
-- `AnimatePresence`의 자식 요소가 React 트리에서 제거될 때 애니메이션을 제거할 수 있다 (`setTimeout()` 함수와 같이 unmount를 요소에 알리고 애니메이션이 완료될 때까지 연기시킨다)
+- `AnimatePresence`의 자식 요소가 React 트리에서 제거될 때 애니메이션을 제거할 수 있다 (`setTimeout()` 함수와 같이 unmount를 요소에 알리고 애니메이션이 완료될 때까지 지연시킨다)
 
 - 자식 요소인 각 `motion` 컴포넌트들은 고유한 `key`값이 있어야, 트리에서 `AnimatePresence`가 컴포넌트의 존재를 추적할 수 있다
+  따라서, `key`값만을 활용하여 컴포넌트의 `exit` 애니메이션을 활성화할 수 있다
--- src/pages/Slider.tsx
+++ src/pages/Slider.tsx
@@ -3,39 +3,43 @@ import { motion, AnimatePresence } from "framer-motion";
 import styled from "styled-components";
 
 export default function Slider() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [idxVisible, setIdxVisible] = useState(1);
+  const nextPlease = () =>
+    setIdxVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const prevPlease = () => setIdxVisible((prev) => (prev === 1 ? 1 : prev - 1));
 
   const boxVariants = {
-    initial: {
+    invisible: {
+      x: 500,
       opacity: 0,
       scale: 0,
     },
     visible: {
+      x: 0,
       opacity: 1,
       scale: 1,
-      rotateZ: 360,
+      transition: {
+        duration: 1,
+      },
     },
-    leaving: {
-      opacity: 0,
-      scale: 0,
-      y: 100,
-    },
+    exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
   };
 
   return (
     <StWrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing && (
-          <StBox
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        )}
+        <StBox
+          variants={boxVariants}
+          initial="invisible"
+          animate="visible"
+          exit="exit"
+          key={idxVisible}
+        >
+          {idxVisible}
+        </StBox>
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </StWrapper>
   );
 }
@@ -51,9 +55,16 @@ const StWrapper = styled.div`
 const StBox = styled(motion.div)`
   width: 400px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+
   position: absolute;
   top: 100px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
